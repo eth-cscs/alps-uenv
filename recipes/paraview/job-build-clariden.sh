20a8eba1bc9e387d4623e50f04a351cfd8f31ace
John Biddiscombe <biddisco@cscs.ch>
2023-08-18 14:57:51 +0200

    Update convenience build scripts for debugging

--- recipes/paraview/job-build-clariden.sh
+++ recipes/paraview/job-build-clariden.sh
@@ -8,7 +8,7 @@
 #SBATCH --output=/users/biddisco/stackinator-output.txt
 #SBATCH --error=/users/biddisco/stackinator-error.txt
 
-CLUSTER=hohgant
+CLUSTER=clariden
 STACKI_DIR=$SRC/alps-vcluster/stackinator
 RECIPE_DIR=$SRC/alps-vcluster/alps-spack-stacks/recipes/paraview
 SYSTEM_DIR=$SRC/alps-vcluster/alps-cluster-config/$CLUSTER
@@ -40,5 +40,5 @@ cp /dev/shm/biddisco/store.squashfs $SCRATCH/$CLUSTER-paraview-$DATE.squashfs
 # -----------------------------------------
 # debug : create a shell using the spack setup used to create the squashfs
 # -----------------------------------------
-# $BUILD_DIR/bwrap-mutable-root.sh --tmpfs ~ --bind $BUILD_DIR/tmp /tmp --bind $BUILD_DIR/store /user-environment env --ignore-environment PATH=/usr/bin:/bin:${BUILD_DIR}/spack/bin SPACK_SYSTEM_CONFIG_PATH=/user-environment/config SPACK_USER_CACHE_PATH=$BUILD_DIR/cache /bin/bash --norc --noprofile
+# $BUILD_DIR/bwrap-mutable-root.sh --tmpfs ~ --bind $BUILD_DIR/tmp /tmp --bind $BUILD_DIR/store /user-environment env --ignore-environment PATH=/usr/bin:/bin:`pwd`/spack/bin SPACK_SYSTEM_CONFIG_PATH=/user-environment/config /bin/bash --norc --noprofile
 
