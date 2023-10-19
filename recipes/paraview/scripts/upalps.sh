31b8a7e88e4e98914e0cf81c72e81fb840d9e691
John Biddiscombe <biddisco@cscs.ch>
2023-10-19 11:28:02 +0200

    Bump paraview to latest 5.11.2 version

--- recipes/paraview/scripts/upalps.sh
+++ recipes/paraview/scripts/upalps.sh
@@ -1,2 +1,3 @@
 /home/biddisco/update-rsync.sh ~/src/alps-vcluster/ clariden:/users/biddisco/src/alps-vcluster
 /home/biddisco/update-rsync.sh ~/src/alps-vcluster/ hohgant:/users/biddisco/src/alps-vcluster
+/home/biddisco/update-rsync.sh ~/src/alps-vcluster/ balfrin:/users/biddisco/src/alps-vcluster
