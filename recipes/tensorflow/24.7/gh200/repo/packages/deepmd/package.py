7ee7831682def390f66b38f22edd1838ec60c11a
bcumming <bcumming@cscs.ch>
2024-07-12 12:04:25 +0200

    clean up env names, working package.py

--- recipes/tensorflow/24.7/gh200/repo/packages/deepmd/package.py
+++ recipes/tensorflow/24.7/gh200/repo/packages/deepmd/package.py
@@ -1,4 +1,3 @@
-
 # Copyright 2013-2024 Lawrence Livermore National Security, LLC and other
 # Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
@@ -8,7 +7,7 @@
 from spack.package import *
 
 
-class Deepmd(PythonPackage, CudaPackage, ROCmPackage):
+class Deepmd(PythonExtension, CudaPackage, ROCmPackage, CMakePackage):
     """DeePMD-kit is a package written in Python/C++, designed to minimize the effort required to build deep learning-based models of interatomic potential energy and force field and to perform molecular dynamics (MD)"""
 
     homepage = "https://docs.deepmodeling.com/projects/deepmd/en/stable/index.html#"
@@ -17,26 +16,61 @@ class Deepmd(PythonPackage, CudaPackage, ROCmPackage):
     license("GNU-LGPL")
     maintainers("mtaillefumier")
 
-    version("3.0.0b0", sha256="44e5a6255f7890f4b9f1cc5e1525380c1d1e41dd75ac9e738d5b81a728759bba")
-    version("3.0.0a0", sha256="36e9dc1b18313139b1e9482e06ec214e15a87ee74f5f9d565b8719998e088920")
-    version("2.2.11", sha256="d22893a08c2556c5cb29682378105849cf672545c91ee52b10a97da6e9075ac3")
-    version("2.2.10", sha256="c31ba1c8a3e874edbbaddc8ec61725a1b74734acb45578f820ab294835d12638")
-    version("2.2.9", sha256="b14419367905b6dd938b9c54f91977c59c25da905a34a7b42585af4805fc4077")
-    version("2.2.8", sha256="21c1e4a38caa81316df574af51f56aa2428dab7b0d71130250e407b81db80f29")
-    version("2.2.7", sha256="25be126de336630493732b09d3b779b89cc916345b22dfb869cc4f3a3ba6dcde")
-    version("2.2.6", sha256="3a4c61d9977b85d1600085dde67d3950f255ea2fcf12e9139d4d369167fce563")
-    version("2.2.5", sha256="1b019f8fbd6d787bd0c03ef0f8160bfd7b6467f2646db5e3bda9153acf8ae878")
-    version("2.2.4", sha256="e39ae25f1545b1cbd84b6122d97278414bbef08e1193a84f827ecdc748b8580b")
+    version(
+        "3.0.0b0",
+        sha256="44e5a6255f7890f4b9f1cc5e1525380c1d1e41dd75ac9e738d5b81a728759bba",
+    )
+    version(
+        "3.0.0a0",
+        sha256="36e9dc1b18313139b1e9482e06ec214e15a87ee74f5f9d565b8719998e088920",
+    )
+    version(
+        "2.2.11",
+        sha256="d22893a08c2556c5cb29682378105849cf672545c91ee52b10a97da6e9075ac3",
+    )
+    version(
+        "2.2.10",
+        sha256="c31ba1c8a3e874edbbaddc8ec61725a1b74734acb45578f820ab294835d12638",
+    )
+    version(
+        "2.2.9",
+        sha256="b14419367905b6dd938b9c54f91977c59c25da905a34a7b42585af4805fc4077",
+    )
+    version(
+        "2.2.8",
+        sha256="21c1e4a38caa81316df574af51f56aa2428dab7b0d71130250e407b81db80f29",
+    )
+    version(
+        "2.2.7",
+        sha256="25be126de336630493732b09d3b779b89cc916345b22dfb869cc4f3a3ba6dcde",
+    )
+    version(
+        "2.2.6",
+        sha256="3a4c61d9977b85d1600085dde67d3950f255ea2fcf12e9139d4d369167fce563",
+    )
+    version(
+        "2.2.5",
+        sha256="1b019f8fbd6d787bd0c03ef0f8160bfd7b6467f2646db5e3bda9153acf8ae878",
+    )
+    version(
+        "2.2.4",
+        sha256="e39ae25f1545b1cbd84b6122d97278414bbef08e1193a84f827ecdc748b8580b",
+    )
 
     variant(
-        "tensorflow", default=True, description="Enable tensorflow support (original ML backend)"
+        "tensorflow",
+        default=True,
+        description="Enable tensorflow support (original ML backend)",
     )
-    variant("pytorch", default=False, description="Enable pytorch support (starting v3.0.0)")
+    variant(
+        "pytorch", default=False, description="Enable pytorch support (starting v3.0.0)"
+    )
     variant("cuda", default=False, description="Enable cuda support")
     variant("rocm", default=False, description="Enable rocm support")
-    variant("lmp", default=False, description="Enable lammps plugins")
     variant("gromacs", default=False, description="Enable gromacs plugins")
-    variant("horovod", default=False, description="Enable horovod support")
+    variant("horovod", default=True, description="Enable horovod support")
+
+    # deepmd library uses cmake as a build system but deepmd itself uses pip.
 
     # Historical dependencies
     depends_on("py-setuptools", type="build")
@@ -49,6 +83,7 @@ class Deepmd(PythonPackage, CudaPackage, ROCmPackage):
     depends_on("py-numpy")
     depends_on("py-pyyaml")
     depends_on("py-args")
+    depends_on("py-pyproject-metadata")
     depends_on("py-python-hostlist@1.21:")
     depends_on("py-typing-extensions", when="python@:3.8")
     depends_on("py-importlib-metadata", when="python@:3.8")
@@ -59,8 +94,9 @@ class Deepmd(PythonPackage, CudaPackage, ROCmPackage):
     depends_on("py-setuptools-scm")
     depends_on("py-scikit-build")
     depends_on("py-hatch-fancy-pypi-readme")
+    depends_on("py-pip", type="build")
     # build lammps with plugin support
-    depends_on("lammps+plugin", when="+lmp")
+    conflicts("py-torch", when="@:3")
 
     # horovod needs some special settings
     depends_on("py-horovod controllers=mpi", when="+horovod")
@@ -72,17 +108,35 @@ class Deepmd(PythonPackage, CudaPackage, ROCmPackage):
         depends_on("py-horovod+cuda tensor_ops=nccl", when="+horovod")
         depends_on("py-tensorflow+cuda+nccl+mpi", when="+tensorflow")
         depends_on("py-torch+cuda", when="+pytorch")
-        depends_on("lammps+cuda", when="+lmp")
     with when("+rocm"):
         depends_on("rccl")
         depends_on("py-horovod+rocm", when="+horovod")
         depends_on("py-tensorflow@2.16:+rocm", when="+tensorflow")
         depends_on("py-torch+rocm", when="+pytorch")
-        depends_on("lammps+rocm", when="+lmp")
+
+    root_cmakelists_dir = "source"
 
     def setup_build_environment(self, env):
         if "+cuda" in self.spec:
             env.set("DP_VARIANT", "cuda")
-            env.set("CMAKE_ARGS", "-DUSE_CUDA_TOOLKIT=ON")
         if "+rocm" in self.spec:
             env.set("DP_VARIANT", "rocm")
+
+    # it is a python package at the origin but we need the c++ library for other projects
+    # pip will install the library but put it in a very incovenient location so we build the library
+    # first as cmake package and then build the package as a python extenstion.
+
+    @run_after("install")
+    def install_python(self):
+        args = std_pip_args + ["--prefix=" + self.prefix, self.stage.source_path]
+        pip(*args)
+
+    def cmake_args(self):
+        spec = self.spec
+        args = [
+            self.define_from_variant("USE_CUDA_TOOLKIT", "cuda"),
+            self.define_from_variant("USE_ROCM_TOOLKIT", "rocm"),
+            self.define_from_variant("USE_TF_PYTHON_LIBS", "tensorflow"),
+        ]
+        return args
+
