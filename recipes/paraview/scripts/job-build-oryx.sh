1bd67472bdb36d3775a2df6119848991136fccc5
John Biddiscombe <biddisco@cscs.ch>
2024-08-29 23:11:30 +0200

    Add cluster name to RC launch script and cleanup script comments etc

--- recipes/paraview/scripts/job-build-oryx.sh
+++ recipes/paraview/scripts/job-build-oryx.sh
@@ -3,38 +3,89 @@
 #SBATCH --job-name=stackinator-paraview
 #SBATCH --time=04:00:00
 #SBATCH --nodes=1
-#SBATCH --partition=nvgpu
+#SBATCH --partition=normal
 #SBATCH --account=csstaff
-#SBATCH --output=/users/biddisco/stackinator-output.txt
-#SBATCH --error=/users/biddisco/stackinator-error.txt
+#SBATCH --output=/users/biddisco/stackinator-output.%j.txt
+#SBATCH --error=/users/biddisco/stackinator-error.%j.txt
+#SBATCH --constraint=mc
 
+# -------------------------------------
+function debug_output() {
+    {
+        echo ""
+        echo "-------------------------------------"
+        echo "-- $1"
+        echo "-------------------------------------"
+    }
+}
+
+# -------------------------------------
 export PYTHONUNBUFFERED=1
 
+# -------------------------------------
+# useful variables
+# -----------------------------------------"
+debug_output "Setup env vars"
 CLUSTER=oryx
+IMAGE=paraview
+ARCH=turing
+VARIANT=osmesa
+VERSION=5.13
+SPACK_ENV_NAME="${IMAGE}-${ARCH}-${VARIANT}-${VERSION}"
+SRC=$HOME/src
 STACKI_DIR=$SRC/alps-vcluster/stackinator
-RECIPE_DIR=$SRC/alps-vcluster/alps-uenv/recipes/paraview/turing
-SYSTEM_DIR=$SRC/alps-vcluster/alps-cluster-config/$CLUSTER
+RECIPE_DIR=$SRC/alps-vcluster/alps-uenv/recipes/${IMAGE}/${ARCH}
+SYSTEM_DIR=$SRC/alps-vcluster/alps-cluster-config/${CLUSTER}
 BUILD_DIR=/dev/shm/biddisco
+DATE=$(date '+%Y-%m-%d')
+SQUASHFS_IMAGE_NAME=$SCRATCH/${SPACK_ENV_NAME}-$DATE.squashfs
 
-echo "Setup/clean build dir"
-#rm -rf   ${BUILD_DIR}/*
+# -----------------------------------------"
+http_proxy=http://proxy.cscs.ch:8080
+https_proxy=$http_proxy
+
+# -----------------------------------------"
+debug_output "Setup/clean build dir"
+rm   -rf ${BUILD_DIR}/*
 mkdir -p ${BUILD_DIR}
 mkdir -p ${BUILD_DIR}/tmp
 
-echo "Execute stackinator"
+# -----------------------------------------"
+debug_output "Execute stackinator"
 $STACKI_DIR/bin/stack-config -s $SYSTEM_DIR -b ${BUILD_DIR} -r $RECIPE_DIR -c $RECIPE_DIR/cache-config.yaml --debug --develop
 
-# build the squashfs image - bubblewrap is used inside the makefile
-echo "Trigger build"
-cd /dev/shm/biddisco
-env --ignore-environment PATH=/usr/bin:/bin:`pwd`/spack/bin make store.squashfs -j32
+# -----------------------------------------"
+debug_output "cd $BUILD_DIR"
+cd $BUILD_DIR
 
-echo "Copy generated squashfs file"
-DATE=$(date +%F)
-cp /dev/shm/biddisco/store.squashfs $SCRATCH/$CLUSTER-paraview-$DATE.squashfs
+# -----------------------------------------"
+debug_output "make squashfs image"
+env --ignore-environment PATH=/usr/bin:/bin:`pwd`/spack/bin HOME="$HOME" http_proxy=$http_proxy https_proxy=$https_proxy no_proxy="$no_proxy" cluster=$CLUSTER make store.squashfs -j32
+
+# -----------------------------------------"
+debug_output "Force push anything that was built successfully"
+env --ignore-environment PATH=/usr/bin:/bin:`pwd`/spack/bin make cache-force
+
+# -----------------------------------------"
+debug_output "check generated squashfs file"
+unalias cp
+if [ -f "$BUILD_DIR/store.squashfs" ]; then
+    echo "Copy generated file to $SQUASHFS_IMAGE_NAME"
+    cp -f $BUILD_DIR/store.squashfs $SQUASHFS_IMAGE_NAME
+else
+    echo "ERROR: $BUILD_DIR/store.squashfs does not exist"
+fi
 
 # -----------------------------------------
 # debug : create a shell using the spack setup used to create the squashfs
 # -----------------------------------------
-# $BUILD_DIR/bwrap-mutable-root.sh --tmpfs ~ --bind $BUILD_DIR/tmp /tmp --bind $BUILD_DIR/store /user-environment env --ignore-environment PATH=/usr/bin:/bin:`pwd`/spack/bin SPACK_SYSTEM_CONFIG_PATH=/user-environment/config /bin/bash --norc --noprofile
+# $BUILD_DIR/bwrap-mutable-root.sh --tmpfs ~ --bind $BUILD_DIR/tmp /tmp --bind $BUILD_DIR/store /user-environment env --ignore-environment PATH=/usr/bin:/bin:`pwd`/spack/bin https_proxy=$https_proxy http_proxy=$http_proxy no_proxy="$no_proxy" SPACK_SYSTEM_CONFIG_PATH=/user-environment/config /bin/bash --norc --noprofile
 
+# -----------------------------------------"
+debug_output "Cleanup /dev/shm directories"
+#rm -rf   ${BUILD_DIR}/*
+
+# -----------------------------------------"
+debug_output "HOWTO: mount the squashfs image for editing"
+echo "unsquashfs -d $BUILD_DIR $SQUASHFS_IMAGE_NAME"
+echo "bwrap --dev-bind / / --bind $BUILD_DIR /user-environment bash"
