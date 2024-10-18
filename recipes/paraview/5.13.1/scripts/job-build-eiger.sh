7de06e4f200e548213d47a62acb626db66d0fb24
John Biddiscombe <biddisco@cscs.ch>
2024-10-18 23:05:08 +0200

    update paraview to 5.13.1, add adios2, h5py and conduit

--- recipes/paraview/5.13.1/scripts/job-build-eiger.sh
+++ recipes/paraview/5.13.1/scripts/job-build-eiger.sh
@@ -7,6 +7,7 @@
 #SBATCH --account=csstaff
 #SBATCH --output=/users/biddisco/stackinator-output.%j.txt
 #SBATCH --error=/users/biddisco/stackinator-error.%j.txt
+#SBATCH --constraint=mc
 
 # -------------------------------------
 function debug_output() {
@@ -25,11 +26,11 @@ export PYTHONUNBUFFERED=1
 # useful variables
 # -----------------------------------------"
 debug_output "Setup env vars"
-CLUSTER=daint
+CLUSTER=eiger
 IMAGE=paraview
-ARCH=gh200
-VARIANT=egl
-VERSION=5.13.0
+ARCH=zen2
+VARIANT=osmesa
+VERSION=5.13.1
 SPACK_ENV_NAME="${IMAGE}-${ARCH}-${VARIANT}-${VERSION}"
 SRC=$HOME/src
 STACKI_DIR=$SRC/alps-vcluster/stackinator
