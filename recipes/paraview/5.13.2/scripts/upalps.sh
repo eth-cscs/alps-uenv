373db54fbb6de66b2c8a280d0589a9344536c0b8
John Biddiscombe <biddisco@cscs.ch>
2025-05-21 14:04:07 +0200

    Remove old sync/update script

--- recipes/paraview/5.13.2/scripts/upalps.sh
+++
@@ -1,2 +0,0 @@
-/home/biddisco/update-rsync.sh ~/src/alps-vcluster/ todi:/users/biddisco/src/alps-vcluster
-/home/biddisco/update-rsync.sh ~/src/alps-vcluster/ eiger:/users/biddisco/src/alps-vcluster
