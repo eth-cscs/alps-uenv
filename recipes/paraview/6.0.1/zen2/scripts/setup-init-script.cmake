f42033f9c8eaa41fafda0bb1e225f664fb962e6d
Alberto Invernizzi <alberto.invernizzi@cscs.ch>
2025-11-05 13:04:46 +0100

    drop gpu wrapper configuration

--- recipes/paraview/6.0.1/zen2/scripts/setup-init-script.cmake
+++ recipes/paraview/6.0.1/zen2/scripts/setup-init-script.cmake
@@ -13,10 +13,3 @@ configure_file(
     ${OUTPUT_DIR}/rc-submit-pvserver.sh
     FILE_PERMISSIONS OWNER_EXECUTE OWNER_WRITE OWNER_READ GROUP_READ GROUP_EXECUTE WORLD_READ WORLD_EXECUTE
     @ONLY)
-
-# perform string substitution on the gpu_wrapper.sh script
-configure_file(
-    ${CMAKE_CURRENT_LIST_DIR}/gpu_wrapper.sh.cmake
-    ${OUTPUT_DIR}/gpu_wrapper.sh
-    FILE_PERMISSIONS OWNER_EXECUTE OWNER_WRITE OWNER_READ GROUP_READ GROUP_EXECUTE WORLD_READ WORLD_EXECUTE
-    @ONLY)
