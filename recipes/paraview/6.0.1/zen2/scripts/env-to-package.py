977d91bfaace737604e02a2d0b554857648eb326
Alberto Invernizzi <alberto.invernizzi@cscs.ch>
2025-11-05 12:20:11 +0100

    minor changes to env-to-package script

--- recipes/paraview/6.0.1/zen2/scripts/env-to-package.py
+++ recipes/paraview/6.0.1/zen2/scripts/env-to-package.py
@@ -11,8 +11,15 @@ import argparse
 # read command line arguments
 parser = argparse.ArgumentParser()
 parser.add_argument("-f", "--filename", help="Path to the yaml file")
-parser.add_argument("-p", "--packagename", default="temppackage", help="Name of the package")
-parser.add_argument("-a", "--add", action="store_true", help="Add the new package directly to the existing spack installation")
+parser.add_argument(
+    "-p", "--packagename", default="temppackage", help="Name of the package"
+)
+parser.add_argument(
+    "-a",
+    "--add",
+    action="store_true",
+    help="Add the new package directly to the existing spack installation",
+)
 args = parser.parse_args()
 
 # create a new directory called 'package'
@@ -23,7 +30,7 @@ packagenameU = args.packagename.replace("-", " ").title().replace(" ", "")
 print(f"Package name: {packagenameU}")
 
 # read the yaml file
-with open(f'{args.filename}', 'r') as stream:
+with open(f"{args.filename}", "r") as stream:
     try:
         env = yaml.safe_load(stream)
     except yaml.YAMLError as exc:
@@ -35,29 +42,32 @@ output += "\n"
 output += "import itertools, os, sys\n"
 output += "from spack.package import *\n"
 output += "\n"
-output +=f"class {packagenameU}(CMakePackage):\n"
-output += "    homepage = \"https://www.dummy.org/\"\n"
-output += "    url      = \"https://www.dummy.org/\"\n"
-output += "    git      = \"https://www.dummy.org/\"\n"
+output += f"class {packagenameU}(CMakePackage):\n"
+output += '    homepage = "https://www.dummy.org/"\n'
+output += '    url      = "https://www.dummy.org/"\n'
+output += '    git      = "https://www.dummy.org/"\n'
 output += "\n"
-output += "    version(\"develop\", branch=\"main\")\n"
+output += '    version("develop", branch="main")\n'
 
-# extract the dependencies from the yaml specs section, assume 'spec' is one level down from top level key 
+# extract the dependencies from the yaml specs section, assume 'spec' is one level down from top level key
 # doign this allows us to import an env from stackinator, or a regular env yaml file
 try:
-    specs = env[list(env.keys())[0]]['specs']
+    specs = env[list(env.keys())[0]]["specs"]
 except KeyError:
     print("No specs found in the yaml file - is 'spec' a top level key?")
     sys.exit(1)
 
 dependencies = []
 for spec in specs:
-    output += f"    depends_on(\"{spec}\")\n"
-    # 
+    output += f'    depends_on("{spec}")\n'
+    #
 print(output)
 
 if args.add:
-    spack_package_root = os.popen('spack location -r').read().strip() + "/var/spack/repos/builtin/packages"
+    spack_package_root = (
+        os.popen("spack location -r").read().strip()
+        + "/var/spack/repos/builtin/packages"
+    )
     tempdir = os.path.join(spack_package_root, args.packagename)
 else:
     # create a subdir named after the package if the subdir doesn't already exist
@@ -65,20 +75,19 @@ else:
 
 if not os.path.exists(tempdir):
     os.makedirs(tempdir)
-print('Writing temp package to ' + os.path.join(tempdir, 'package.py'))
+print("Writing temp package to " + os.path.join(tempdir, "package.py"))
 
 # create a temp file in the directory
-tempfile = os.path.join(tempdir, 'package.py')
-with open(tempfile, 'w') as f:
+tempfile = os.path.join(tempdir, "package.py")
+with open(tempfile, "w") as f:
     f.write(output)
     f.close()
     print(f"Temporary file {tempfile} has been created")
 
 print(f'spackgen {args.packagename} "{args.packagename} %gcc" --reuse')
 
-with open('./repo.yaml', 'w') as f:
-    f.write(f"repo:\n")
-    f.write(f"  namespace: 'userenv'\n")
+with open("./repo.yaml", "w") as f:
+    f.write("repo:\n")
+    f.write("  namespace: 'userenv'\n")
     f.close()
     print("Repo file has been created")
-
