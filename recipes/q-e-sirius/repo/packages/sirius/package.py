6fa9df32eedf0ccee25f41a4ce25c09662ff2654
Simon Pintarelli <simon.pintarelli@cscs.ch>
2025-12-09 14:11:25 +0100

    q-e-sirius@1.0.2

--- recipes/q-e-sirius/repo/packages/sirius/package.py
+++ recipes/q-e-sirius/repo/packages/sirius/package.py
@@ -28,6 +28,7 @@ class Sirius(CMakePackage, CudaPackage, ROCmPackage):
     version("develop", branch="develop")
     version("master", branch="master")
 
+    version("7.10.0", sha256="ef02f2a899ccd72803bed3c4b7460457e67d99a68e0d6e5ab157566a1e6abf4f")
     version("7.9.0", sha256="c36e9a00637b9626f83c0db740751440bfe06ef4c8d7cadb9822dd3cf03c1046")
     version("7.8.0", sha256="2cd2f98d35fb9e0a8f6d68714c6f8d682895781d564e91ef6685d92569ffd413")
     version("7.7.1", sha256="6039c84197d9e719e826f98b840cff19bc513887b443f97c0099d3c8b908efed")
