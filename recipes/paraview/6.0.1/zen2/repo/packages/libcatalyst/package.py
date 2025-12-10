3f01cfd0855cc956292abd98447862813d683d5a
Alberto Invernizzi <alberto.invernizzi@cscs.ch>
2025-12-10 14:34:22 +0100

    conduit fix in libcatlayst and other small changes

--- recipes/paraview/6.0.1/zen2/repo/packages/libcatalyst/package.py
+++ recipes/paraview/6.0.1/zen2/repo/packages/libcatalyst/package.py
@@ -25,30 +25,32 @@ class Libcatalyst(CMakePackage):
     version("2.0.0", sha256="5842b690bd8afa635414da9b9c5e5d79fa37879b0d382428d0d8e26ba5374828")
 
     variant("mpi", default=False, description="Enable MPI support")
-    variant("conduit", default=False, description="Use external Conduit for Catalyst")
     variant("fortran", default=False, description="Enable Fortran wrapping")
     variant("python", default=False, description="Enable Python wrapping")
 
-    depends_on("c", type="build")  # generated
-    depends_on("cxx", type="build")  # generated
-    depends_on("fortran", type="build")  # generated
+    depends_on("c", type="build")
+    depends_on("cxx", type="build")
+    depends_on("fortran", type="build", when="+fortran")
+
+    depends_on("cmake@3.26:", type="build")
     depends_on("pkgconfig", type="build")
 
     depends_on("mpi", when="+mpi")
-    depends_on("conduit", when="+conduit")
-    depends_on("cmake@3.26:", type="build")
+    depends_on("conduit")
+
+    depends_on("conduit+fortran", when="+fortran")
 
     with when("+python"):
         extends("python")
-        depends_on("python@3:")
         depends_on("py-numpy", type=("build", "link", "run"))
+        depends_on("conduit+python", type=("build", "link", "run"))
 
     def cmake_args(self):
         """Populate cmake arguments for libcatalyst."""
         args = [
-            "-DCATALYST_BUILD_TESTING=OFF",
+            self.define("CATALYST_BUILD_TESTING", False),
+            self.define("CATALYST_WITH_EXTERNAL_CONDUIT", True),
             self.define_from_variant("CATALYST_USE_MPI", "mpi"),
-            self.define_from_variant("CATALYST_WITH_EXTERNAL_CONDUIT", "conduit"),
             self.define_from_variant("CATALYST_WRAP_FORTRAN", "fortran"),
             self.define_from_variant("CATALYST_WRAP_PYTHON", "python"),
         ]
